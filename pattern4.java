035e0a98aa7441c4293face97a0b583959325fa2
ANUSHKA102 <68914425+ANUSHKA102@users.noreply.github.com>
2021-10-09 00:58:32 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,27 @@
+/*
+Pattern 4
+Take no of rows as input then print like-
+for n=5:
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+*/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	       for(int j=0;j<n;j++){
+	           for(int k=0;k<n-j;k++){
+	               System.out.print("* ");
+	           }
+	       System.out.println();
+	}
+}
+}
