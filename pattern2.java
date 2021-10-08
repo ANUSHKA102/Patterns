f4b31c0f11cd76d4a8bd7747ef591a6f6b5a949d
ANUSHKA102 <68914425+ANUSHKA102@users.noreply.github.com>
2021-10-09 01:28:50 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -26,3 +26,34 @@ public class Main
 	    }
 	}
 }
+
+
+
+
+
+
+
+
+
+Second Solution
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	       for(int j=0;j<n;j++){
+	           for(int k=0;k<n;k++){
+	               if(j>=k){
+	                   System.out.print("* ");
+	               }
+	               else{
+	                    System.out.print(" ");
+	                   
+	               }
+	           }
+	       System.out.println();
+	}
+}
+}
