a0cb5cb5734c57c96d7cee4418ee6f62613910c9
ANUSHKA102 <68914425+ANUSHKA102@users.noreply.github.com>
2021-10-09 01:11:01 +0530

    Update pattern4.java

--- pattern4.java
+++ pattern4.java
@@ -25,3 +25,28 @@ public class Main
 	}
 }
 }
+
+
+
+
+
+
+
+Second Solution
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	       for(int j=0;j<n;j++){
+	           for(int k=j;k<n;k++){
+	               System.out.print("* ");
+	           }
+	       System.out.println();
+	}
+}
+}
+
