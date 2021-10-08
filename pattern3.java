b9fee23818881516ddfdf4901d35b72b84df8585
ANUSHKA102 <68914425+ANUSHKA102@users.noreply.github.com>
2021-10-09 00:45:57 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,41 @@
+/*
+Pattern 1
+Take no of rows as input then print like-
+for n=5:
+
+    *
+   **
+  ***
+ ****
+*****
+
+
+*/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	  
+	       for(int j=0;j<n;j++){
+	           for(int k=0;k<n;k++){
+	               if(k<n-j-1){
+	                   System.out.print(" ");
+	               }
+	               else{
+	                   System.out.print("*");
+	               }
+	              
+	           }
+	           System.out.println();
+	       
+	           
+	           
+	       }
+	           System.out.println();
+	       
+	        
+	    
+	}
+}
