44fe60609f9ba30177beab2ca9b6b86e31578b1f
ANUSHKA102 <68914425+ANUSHKA102@users.noreply.github.com>
2021-10-10 11:32:55 +0530

    Add files via upload

---
+++ pyramid 2.java
@@ -0,0 +1,35 @@
+/*
+Pattern 12
+Take no of rows as input then print like-
+for n=5:
+        *
+      *   *
+    *   *   *
+  *   *   *   *
+*   *   *   *    *
+   
+  
+
+
+*/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    for(int j=0;j<n;j++){
+		        if(j<n-i-1){
+		            System.out.print(" ");
+		        }
+		        else{
+		            System.out.print("* ");
+		        }
+		        
+		    }
+		     System.out.println();
+		    
+		}
+	}
+}
